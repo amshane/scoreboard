aac79d85e63e460a5647b575c64f416444499e58
amshane <allisonmandy90@gmail.com>
2015-05-26 21:37:21 -0400

    erb finished without html tags or css

---
+++ app/models/scoreboard.rb
@@ -0,0 +1,17 @@
+# require 'nokogiri'
+
+# class Scoreboard
+
+# 	# attr_accessor :mlb
+
+# 	def initialize
+# 		# @mlb = File.open('mlb.json')
+# 		mlb_info
+# 	end
+	
+# 	def mlb_info
+# 		f = File.open('mlb.json')
+# 		Nokogiri::HTML(f)
+# 	end
+
+# end
